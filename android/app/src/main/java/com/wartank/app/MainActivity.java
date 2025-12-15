d97d437299c82b95272431b7bd68764cf24ea3f9
ImprimirComandas <impressaocomandas@gmail.com>
2025-12-15 08:45:16 +0000

    aaddcapacitor

---
+++ android/app/src/main/java/com/wartank/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.wartank.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.wartank.app',
+  appName: 'War Tank Arena',
+  webDir: 'dist'
+};
+
+export default config;
